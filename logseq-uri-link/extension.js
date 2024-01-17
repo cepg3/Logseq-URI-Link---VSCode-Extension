51dd1be2d09048cda26c73e36fab32595cf53916
gagnon.ce <gagnon.ce@stas.com>
2024-01-17 15:41:45 -0500

    Feat : Create the extension's js

--- logseq-uri-link/extension.js
+++ logseq-uri-link/extension.js
@@ -1,36 +1,30 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const { languages, DocumentLink, Range, Uri } = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+class CustomDocumentLinkProvider {
+    provideDocumentLinks(document, token) {
+        const text = document.getText();
+        const customLinkRegex = /\blogseq:\/\/\S*\b/g;
+        let match;
+        const links = [];
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+        while ((match = customLinkRegex.exec(text)) !== null) {
+            const startPos = document.positionAt(match.index);
+            const endPos = document.positionAt(match.index + match[0].length);
+            const range = new Range(startPos, endPos);
+            const uri = Uri.parse(match[0]);
+            const link = new DocumentLink(range, uri);
+            links.push(link);
+        }
+
+        return links;
+    }
+}
+
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "logseq-uri-link" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('logseq-uri-link.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Logseq URI Link!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(languages.registerDocumentLinkProvider({ scheme: 'file' }, new CustomDocumentLinkProvider()));
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+exports.activate = activate;
 
-module.exports = {
-	activate,
-	deactivate
-}
