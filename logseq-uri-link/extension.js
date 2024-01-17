81ce99964506073020b9c43b8559bedb64fa2a4c
gagnon.ce <gagnon.ce@stas.com>
2024-01-17 16:15:09 -0500

    Fix : Make shit work

--- logseq-uri-link/extension.js
+++ logseq-uri-link/extension.js
@@ -1,30 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const { languages, DocumentLink, Range, Uri } = require('vscode');
+const { languages, CodeLens, Range, Uri, commands, env } = require('vscode');
 
-class CustomDocumentLinkProvider {
-    provideDocumentLinks(document, token) {
+class CustomCodeLensProvider {
+    provideCodeLenses(document, token) {
         const text = document.getText();
         const customLinkRegex = /\blogseq:\/\/\S*\b/g;
         let match;
-        const links = [];
+        const lenses = [];
 
         while ((match = customLinkRegex.exec(text)) !== null) {
             const startPos = document.positionAt(match.index);
             const endPos = document.positionAt(match.index + match[0].length);
             const range = new Range(startPos, endPos);
             const uri = Uri.parse(match[0]);
-            const link = new DocumentLink(range, uri);
-            links.push(link);
+            const lens = new CodeLens(range, {
+                title: "Open Link",
+                command: "extension.openLink",
+                arguments: [uri]
+            });
+            lenses.push(lens);
         }
 
-        return links;
+        return lenses;
     }
 }
 
 function activate(context) {
-    context.subscriptions.push(languages.registerDocumentLinkProvider({ scheme: 'file' }, new CustomDocumentLinkProvider()));
+    context.subscriptions.push(languages.registerCodeLensProvider({ scheme: 'file' }, new CustomCodeLensProvider()));
+
+    context.subscriptions.push(commands.registerCommand('extension.openLink', async (uri) => {
+        // Open the link with the default browser
+        await env.openExternal(uri);
+    }));
 }
 
 exports.activate = activate;
-
